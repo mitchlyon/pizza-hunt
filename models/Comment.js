af919355f7b15210e92ade6411616a874db145f1
Mitchell Lyon <kekoa.lyon@gmail.com>
2021-11-16 22:00:52 -0700

    lesson 3

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,60 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        replies: [ReplySchema]
     },
-    commentBody: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
+);
+
+CommentSchema.virtual('replyCount').get(function () {
+    return this.replies.length;
 });
 
-const Comment =  model('Comment', CommentSchema);
+
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const Comment = model('Comment', CommentSchema);
 
 module.exports = Comment;
